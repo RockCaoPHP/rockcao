8ab9fa4905db3bebd97377047f482d7dfc5a6d67
caoyongjing <549777430@qq.com>
2020-07-19 01:51:23 +0800

    TOKEN

--- application/wechat/controller/Notify.php
+++ application/wechat/controller/Notify.php
@@ -3,8 +3,33 @@ namespace app\wechat\controller;
 
 class Notify
 {
+	const TOKEN = 'CSC@2018@LL';
+	
 	public function index()
 	{
-		echo 123;
+		$this->checkSign();
+		
+		echo 'OK';
+	}
+	
+	private function checkSign()
+	{
+		$signature = input('get.signature');
+		$timestamp = input('get.timestamp');
+		$nonce = input('get.nonce');
+		$echostr = input('get.echostr');
+		
+		$sign = [self::TOKEN, $timestamp, $nonce];
+		sort($sign, SORT_STRING);
+		$sign = implode($sign);
+		$sign = sha1($sign);
+		
+		($sign === $signature) or exit('签名错误');
+		
+		if ($echostr) {
+			exit($echostr);
+		}
+		
+		return true;
 	}
 }
